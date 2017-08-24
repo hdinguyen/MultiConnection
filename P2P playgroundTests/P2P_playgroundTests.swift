4252f026785b6d91692ee6b852ce3777698df21a
nguyen huynh <nguyen.huynh@butterflyhub.com>
2017-08-24 15:57:10 +0700

    Init

---
+++ P2P playgroundTests/P2P_playgroundTests.swift
@@ -0,0 +1,36 @@
+//
+//  P2P_playgroundTests.swift
+//  P2P playgroundTests
+//
+//  Created by Nguyenh on 8/24/17.
+//  Copyright © 2017 nguyenh. All rights reserved.
+//
+
+import XCTest
+@testable import P2P_playground
+
+class P2P_playgroundTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
